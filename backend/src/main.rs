6d6940af615963649fab6034eb8caa5b20730294
Sneekyboots <sneekyboots122@gmail.com>
2025-07-26 12:08:32 +0000

    test basics

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,13 @@
-use axum::{Router, response::Html, routing::get};
+use axum::{Router, response::Html, routing::get,Json};
+use serde_json::json;
 
 #[tokio::main]
 async fn main() {
     println!("🚀 Starting Artchain backend server...");
     let app = Router::new()
         .route("/", get(home))
-        .route("/api/health", get(health_check));
+        .route("/api/health", get(health_check))
+        .route("/api/nfts",get(list_nfts));
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
@@ -21,4 +23,23 @@ async fn home() -> Html<&'static str> {
 }
 async fn health_check() -> &'static str {
     "Artchain API is healthy and running smooth"
+}
+async fn list_nfts()->Json<serde_json::Value>{
+    Json(json!({
+        "nfts":[
+            {
+                "id":1,
+                "name":"Cool Art #1",
+                "price":0.6,
+            "artist":"Artist1"            },
+            {
+                "id":2,
+                "name":"Digital MasterPiece",
+                "price":1.2,
+            "artist":"Artist2" 
+            }
+            
+        ],
+        "total":2
+    }))
 }
